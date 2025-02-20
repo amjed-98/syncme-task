189e2d29a282efa1fecb271f4043b24e860724be
Amjad Yahia <amjadyahia.dev@gmail.com>
2025-02-20 14:03:48 +0200

    remove sleep function

--- src/handlers/process.ts
+++ src/handlers/process.ts
@@ -2,15 +2,13 @@ import { DB_TABLES } from "../config";
 import { DBService } from "../services/db.service";
 import { ImportStatusService } from "../services/import-status.service";
 import { WooCommerceService } from "../services/woo-commerce.service";
-import { ErrorResponse, sleep, SuccessResponse } from "../utils";
+import { ErrorResponse, SuccessResponse } from "../utils";
 
 export async function handler() {
   try {
     await ImportStatusService.update("in-progress");
 
     const categories = await WooCommerceService.fetchCategories();
-
-    await sleep(2000);
 
     await DBService.batchWrite(
       DB_TABLES.categories,
